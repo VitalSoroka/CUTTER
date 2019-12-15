511b3da1139cd87beadd979318538dd493251457
VitalSoroka <33748241+VitalSoroka@users.noreply.github.com>
2019-12-15 15:24:56 +0300

    added method for Attack

--- src/main/java/com/mycompany/model/method/CutterMethod.java
+++ src/main/java/com/mycompany/model/method/CutterMethod.java
@@ -7,7 +7,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class CutterMethod {
-    private static final double SIGNAL_ENERGY_DEFAULT = 0.31;
+    private static final double SIGNAL_ENERGY_DEFAULT = 0.1;
     private static final int STEP_DEFAULT = 10;
     private static final int CROSS_SIZE = 2;
     private int step;
@@ -39,6 +39,7 @@ public class CutterMethod {
             int blueComponent = colorPixel.getBlue();
 
             int newBlueComponent = blueComponent + (int) (signalEnergy * ImageUtils.getBrightsForPixel(colorPixel) * (2 * (bite - 48) - 1) );
+            newBlueComponent = Math.max(Math.min(0xFF, newBlueComponent), 0);
 
             colorPixel = new Color(colorPixel.getRed(), colorPixel.getGreen(), newBlueComponent);
             image.setRGB(pixelPosition.getX(), pixelPosition.getY(), colorPixel.getRGB());
@@ -48,10 +49,11 @@ public class CutterMethod {
     }
 
     public String retrieveMessage(BufferedImage image) {
-        return retrieveMessage(image, 1);
+        return retrieveMessage(image, step);
     }
 
-    public String retrieveMessage(BufferedImage image, int step) {
+    @SuppressWarnings("Duplicates")
+    public String retrieveMessage(BufferedImage image, int step){
         StringBuffer binaryMessage = new StringBuffer();
         int numberPixel = 1;
         int quantityContainers = (int) Math.pow(Math.min(image.getHeight(), image.getWidth()) - (2 * CROSS_SIZE), 2);
@@ -66,9 +68,35 @@ public class CutterMethod {
             }
             int avgBlueComponent = (sumOfBlueComponentInCross - 2 * blueComponent) / (CROSS_SIZE * 4);
 
-            if ((blueComponent - avgBlueComponent) > (signalEnergy * 0.6 *  ImageUtils.getBrightsForPixel(colorPixel)))
+            if ((blueComponent - avgBlueComponent) > 0)
                 binaryMessage.append('1');
-            if ((avgBlueComponent - blueComponent) > (signalEnergy * 0.6 *  ImageUtils.getBrightsForPixel(colorPixel)))
+            if ((avgBlueComponent - blueComponent) > 0)
+                binaryMessage.append('0');
+            numberPixel += step;
+        }
+        return binaryMessage.toString();
+    }
+
+
+    @SuppressWarnings("Duplicates")
+    public String attack(BufferedImage image, int step) {
+        StringBuffer binaryMessage = new StringBuffer();
+        int numberPixel = 1;
+        int quantityContainers = (int) Math.pow(Math.min(image.getHeight(), image.getWidth()) - (2 * CROSS_SIZE), 2);
+        while (numberPixel <= quantityContainers) {
+            Position pixelPosition = ImageUtils.getPositionForSnakeWay(numberPixel, CROSS_SIZE);
+            Color colorPixel = new Color(image.getRGB(pixelPosition.getX(), pixelPosition.getY()));
+            int blueComponent = colorPixel.getBlue();
+            int sumOfBlueComponentInCross = 0;
+            for (int i = -CROSS_SIZE; i <= CROSS_SIZE; i++) {
+                sumOfBlueComponentInCross += new Color(image.getRGB(pixelPosition.getX() + i, pixelPosition.getY())).getBlue();
+                sumOfBlueComponentInCross += new Color(image.getRGB(pixelPosition.getX(), pixelPosition.getY() + i)).getBlue();
+            }
+            int avgBlueComponent = (sumOfBlueComponentInCross - 2 * blueComponent) / (CROSS_SIZE * 4);
+
+            if ((blueComponent - avgBlueComponent) > (signalEnergy * 0.05 *  ImageUtils.getBrightsForPixel(colorPixel)))
+                binaryMessage.append('1');
+            if ((avgBlueComponent - blueComponent) > (signalEnergy * 0.05 *  ImageUtils.getBrightsForPixel(colorPixel)))
                 binaryMessage.append('0');
             numberPixel += step;
         }
