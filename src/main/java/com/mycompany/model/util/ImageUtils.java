671e7b93e4cac5b8077fdc994f5a6f47b56878fd
VitalSoroka <33748241+vitalsoroka@users.noreply.github.com>
2019-12-15 15:33:13 +0300

    added method for additions noise and methods for cleaning LSB

--- src/main/java/com/mycompany/model/util/ImageUtils.java
+++ src/main/java/com/mycompany/model/util/ImageUtils.java
@@ -1,8 +1,12 @@
 package com.mycompany.model.util;
 
 import com.mycompany.model.entity.Position;
+import ij.process.ImageProcessor;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.Date;
+import java.util.Random;
 
 public class ImageUtils {
     private static final double RED_WEIGHT = 0.299d;
@@ -13,10 +17,46 @@ public class ImageUtils {
         return RED_WEIGHT * pixel.getRed() + GREEN_WEIGHT * pixel.getGreen() + BLUE_WEIGHT * pixel.getBlue();
     }
 
-    public static Position getPositionForSnakeWay(int blockNumber, int borderOffset,  int width, int heigh){
+    public static Position getPositionForSnakeWay(int blockNumber, int borderOffset, int width, int heigh) {
         Position position = getPositionForSnakeWay(blockNumber, borderOffset);
-        return new Position((position.getX() - borderOffset) * width + borderOffset, (position.getY() - borderOffset) * heigh +borderOffset);
+        return new Position((position.getX() - borderOffset) * width + borderOffset, (position.getY() - borderOffset) * heigh + borderOffset);
     }
+
+    public static void clearLSB(BufferedImage image) {
+        for (int i = 0; i < image.getHeight(); i++) {
+            for (int j = 0; j < image.getWidth(); j++) {
+                image.setRGB(image.getRGB(i, j) & 0xFFFEFEFE, i, j);
+
+            }
+        }
+    }
+
+    public static void clearTwoLSB(BufferedImage image) {
+        for (int i = 0; i < image.getHeight(); i++) {
+            for (int j = 0; j < image.getWidth(); j++) {
+                image.setRGB(image.getRGB(i, j) & 0xFFFCFCFC, i, j);
+
+            }
+        }
+    }
+
+    public static void addNoise(BufferedImage image, float volume) {
+        int amount = Math.round(volume * 0xFF / 100);
+        Random generator = new Random(new Date().getTime());
+        for (int i = 0; i < image.getHeight(); i++) {
+            for (int j = 0; j < image.getWidth(); j++) {
+                Color pixelColr = new Color(image.getRGB(i, j));
+                int noiseForBlue = generator.nextInt(amount + 1);
+                int noiseForGreen = generator.nextInt(amount + 1);
+                int noiseForRed = generator.nextInt(amount + 1);
+                Color pixelWithNoise = new Color(Math.min(0xFF, pixelColr.getRed() + noiseForRed),
+                                                 Math.min(0xFF, pixelColr.getGreen() + noiseForGreen),
+                                                 Math.min(0xFF, pixelColr.getBlue() + noiseForBlue));
+                image.setRGB(i, j, pixelWithNoise.getRGB());
+            }
+        }
+    }
+
 
     @SuppressWarnings("Duplicates")
     public static Position getPositionForSnakeWay(int blockNumber, int borderOffset) {
